b9a22fa44bbd6f73c779c8c161a1aefbe98d02fa
cdelmoral <carderomad@gmail.com>
2015-04-19 22:11:32 -0500

    add exercise_02.js

---
+++ exercise_02.js
@@ -0,0 +1,11 @@
+/**
+ * You will get a file as the first argument to your program
+ * (process.argv[2]).
+ * 
+ * Use `fs.createReadStream()` to pipe the given file to `process.stdout`.
+ */
+var fs = require('fs');
+
+var file = process.argv[2];
+
+fs.createReadStream(file).pipe(process.stdout);
